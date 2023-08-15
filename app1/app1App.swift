339f99b073fb3cb1ad2055b7dc02d8fcfcf69ab0
Miys <supergirlmiya@gmail.com>
2023-08-15 18:27:21 +0100

    Initial Commit

---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by Miya  on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
